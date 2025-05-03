2ab7bcb2e9f9ad7adf75c8a1fdbd81c6860bacc8
Muhammad Salman <salmannawaz008@gmail.com>
2025-05-03 21:57:35 +0500

    updating global.css import

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@
 
 import { UserState } from "@/context/UserContext";
 import "@ant-design/v5-patch-for-react-19";
-import "./globals.css";
+import "@/app/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (

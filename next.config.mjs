44b9857b5285bb4a37d714fa3ae7ecc0e5997484
Muhammad Salman <salmannawaz008@gmail.com>
2025-05-12 01:24:40 +0500

    updated next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // For hybrid Pages+App router
+  experimental: {
+    appDir: true,
+  },
   middleware: "./src/middleware.js", // Explicit path
 };
 

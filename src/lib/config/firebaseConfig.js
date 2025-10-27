00c4145917410b9084fd7c3a27de238b62c6a134
Salman <salmannawaz008@gmail.com>
2025-10-28 00:36:27 +0500

    updates: firebase config

--- src/lib/config/firebaseConfig.js
+++ src/lib/config/firebaseConfig.js
@@ -3,19 +3,32 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+let app;
+
+export const getFirebaseApp = () => {
+  if (!getApps().length) {
+    const firebaseConfig = {
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    };
+
+    if (!firebaseConfig.apiKey)
+      throw new Error(
+        "Missing Firebase API key. Check your environment variables."
+      );
+
+    app = initializeApp(firebaseConfig);
+  } else {
+    app = getApp();
+  }
+  return app;
 };
 
-// Prevent duplicate app initialization during hot reload
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-export const auth = getAuth(app);
+export const db = getFirestore(getFirebaseApp());
+export const auth = getAuth(getFirebaseApp());
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
